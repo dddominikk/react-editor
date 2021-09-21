96e63954d6af045dbde75d8ab4be643795e46a07
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2021-09-21 15:44:56 +0200

    Create exports.js

---
+++ src/exports.js
@@ -0,0 +1,4 @@
+export {CSS} from './app.css';
+export {default as Header} from './components/header/header.jsx';
+export {default as Textarea} from './components/editor/textarea.jsx';
+export {default as Navbar} from './components/nav/navbar.jsx';
