eb59efcc571e89a616893a6c39e4fa584fb7c6af
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2021-09-21 16:36:33 +0200

    added Footer to global exports

--- src/exports.js
+++ src/exports.js
@@ -1,4 +1,5 @@
 export {CSS} from './app.css';
 export {default as Header} from './components/header/header.jsx';
+export {default as Footer} from './components/footer/footer.jsx';
 export {default as Textarea} from './components/editor/textarea.jsx';
 export {default as Navbar} from './components/nav/navbar.jsx';
