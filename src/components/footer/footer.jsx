a8f478df6d7e5077fc59ab2e260991becb0a4886
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2021-09-21 16:32:48 +0200

    added dynamic footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './footer.css';
+
+const footerContents = () => {
+  
+  const 
+        start = 2021,
+        end = new Date(Date.now()).getFullYear(),
+        cRange = (!end-start) ? `${end}` : `${start}-${end}`,
+        result = `${cRange}©️Attention Spa, vl. Dominik Bošnjak`;
+  
+  return result;
+};
+
+
+const Footer = () => {
+
+    return (
+      <footer>
+      <p>{footerContents()}</p>
+      </footer>
+    );
+
+};
+ 
+export default Footer;
